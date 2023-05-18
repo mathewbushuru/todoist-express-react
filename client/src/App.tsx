5c540b617a61ced26802fd2600f6b2a4bd912af7
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-18 01:58:36 -0700

    react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Todoist Clone App</h1>,
+  },
+]);
+
 function App() {
-  return <div className="">Todoist</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
