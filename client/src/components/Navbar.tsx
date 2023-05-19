8c3993ec8d0b588269061369585ba88f43266a6a
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-18 23:47:10 -0700

    Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,18 +4,30 @@ import { MenuIcon } from "lucide-react";
 import Logo from "@/components/ui/Logo";
 import Button from "@/components/ui/Button";
 
+const navItems = ["Features", "Templates", "For Teams", "Resources", "Pricing"];
+
 const Navbar: FC = ({}) => {
   return (
-    <div className="flex items-center justify-between border-b p-4 lg:px-16 lg:border-b-0">
+    <div className="flex items-center justify-between border-b p-4 lg:px-8 xl:px-16 lg:border-b-0">
       <Logo />
       <MenuIcon className="lg:hidden" />
-      <div className="hidden lg:flex gap-8">
-        <Button>Default</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button size="sm">Small</Button>
-        <Button size="lg">Large</Button>
+      <div className="hidden lg:flex lg:items-center xl:gap-3">
+        {navItems.map((item) => (
+          <Button
+            variant="ghost"
+            key={item}
+            className="xl:text-lg font-normal tracking-wide"
+          >
+            {item}
+          </Button>
+        ))}
+        <div className="w-[1.5px] h-full bg-secondary">&nbsp;</div>
+        <Button variant="ghost" className="xl:text-lg font-normal tracking-wide">
+          Log in
+        </Button>
+        <Button className="xl:text-lg font-normal tracking-wide">
+          Start for free
+        </Button>
       </div>
     </div>
   );
