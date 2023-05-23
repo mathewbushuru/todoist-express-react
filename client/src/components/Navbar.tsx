461f3e89c42e713bdbb2f8d4d8499fb58ddf01d3
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-22 22:10:36 -0700

    fix: navbar text btn sizing in xl screens

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar: FC = ({}) => {
           <Button
             variant="ghost"
             key={item}
-            className="font-normal tracking-wide xl:text-lg xl:text-[18px]"
+            className="font-normal tracking-wide xl:text-[16px] "
           >
             {item}
           </Button>
@@ -24,11 +24,11 @@ const Navbar: FC = ({}) => {
         <div className="h-full w-[1.5px] bg-secondary">&nbsp;</div>
         <Button
           variant="ghost"
-          className="font-normal tracking-wide xl:text-lg xl:text-[18px]"
+          className="font-normal tracking-wide xl:text-[16px]"
         >
           Log in
         </Button>
-        <Button className="font-normal tracking-wide xl:text-lg xl:text-[18px]">
+        <Button className="font-normal tracking-wide xl:text-[16px]">
           Start for free
         </Button>
       </div>
