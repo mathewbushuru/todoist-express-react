4187f9c07e94e5c42578a47a3566666d3072313e
mathew bushuru <bushuruwafulamathew@gmail.com>
2023-05-29 16:19:09 -0700

    removed compiler dist

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 3001;
-app_1.app.listen(PORT, () => {
-    console.log(`Todoist server listening at PORT ${PORT}`);
-});
--- server/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const router = (0, express_1.Router)();
-router.post("/login", auth_1.loginController);
-router.post("/signup", auth_1.signupController);
-exports.default = router;
